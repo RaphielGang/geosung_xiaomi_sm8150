bc6821eea041d198eb6dc13d675c20a6dbaa2bc0
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2019-07-16 19:21:02 +0700

    Treewide / Frickening : Remove Xiaomi Copyright Header for now
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -2,7 +2,6 @@
  * Contains CPU specific errata definitions
  *
  * Copyright (C) 2014 ARM Ltd.
- * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
